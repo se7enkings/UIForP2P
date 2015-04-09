c04fecb9644a2c095f947d2178537840e5c9900b
Shiyi Liang <liangshiyi920105@hotmail.com>
2015-04-09 20:28:07 +0800

    init

---
+++ js/Settings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by GT7_Liang on 15/4/2.
+ */
+function UI_User_Volume_ChangeFunc()
+{
+    var num_get=document.getElementById("UI_User_Volume_Change");
+    var num_show=document.getElementById("UI_User_Volume_Show");
+    num_show.value=num_get.value;
+}
